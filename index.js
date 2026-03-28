1bf6e3f9d38a95cb6b352e0be81ad8164b2600f4
lmtca07tha <lmtca07tha@gmail.com>
2026-03-28 11:10:00 +0700

    init blog

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+var a = 1;
+var b = 2;
+
+var c = a + b;
+
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
